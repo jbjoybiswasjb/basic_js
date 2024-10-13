be7fa350d9f86c7b37ce2712eca16886b6dc9104
jbjoybiswasjb <jbjoybiswasjb@gmail.com>
2024-10-13 20:57:18 +0600

    first commit

---
+++ js.js
@@ -0,0 +1,37 @@
+/* 
+1. Variable
+===============================================================================================
+*/
+
+// How to declare a variable.
+// keyword variableName = variableValue;
+
+
+// 3 Case of variable naming convention.
+/*
+
+1. snake_case
+2. PascalCase
+3. camelCase
+4. UPPER_SNAKE_CASE
+
+*/
+
+
+// Variable.
+// Number, String, Boolean, Array, Object, Null, Undefined.
+
+let nickAge = 1;
+let nicksPetName = 'Zo';
+let isPassed = true;
+const NICK_FRIENDS = ['Selena', 'Priyanka', 'Gomez', 'Chopra'];
+const NICK_DETAILS = {
+    name: 'Nick',
+    age: 29,
+    girlFriends: ['Honey', 'Selena Gomez'],
+    car: {
+        firstCar : 'Toyota',
+        price: 2300000,
+        color: 'Black',
+    }
+}
